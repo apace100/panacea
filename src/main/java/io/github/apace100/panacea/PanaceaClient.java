d23937f56b21c23d9f9b412115cdd6f7528bd643
Apace <apace100@web.de>
2020-06-08 19:10:30 +0200

    Added PanaceaClient file which was left behind

--- src/main/java/io/github/apace100/panacea/PanaceaClient.java
+++ src/main/java/io/github/apace100/panacea/PanaceaClient.java
@@ -3,12 +3,15 @@ package io.github.apace100.panacea;
 import io.github.apace100.panacea.registry.ModBlocks;
 import io.github.apace100.panacea.registry.ModScreens;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
 
 public class PanaceaClient implements ClientModInitializer {
 
     @Override
+    @Environment(EnvType.CLIENT)
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LAVONG, RenderLayer.getTranslucent());
         ModScreens.register();
